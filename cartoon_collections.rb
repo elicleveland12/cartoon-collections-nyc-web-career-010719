5625aa9493ad7eecf53a762fc3127cbcd4e6f625
Eli Cleveland <elicleveland12@gmail.com>
2019-01-02 14:50:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,8 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |i|
-    
+    i == cheese_types.each do |j|
+      return j
+    end 
+  end
 end
